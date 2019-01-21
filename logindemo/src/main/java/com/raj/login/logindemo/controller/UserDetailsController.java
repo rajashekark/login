96278afb1d52ec27b4c8765bb6be11c057105199
Rajashekar <rajashekarreddy339@gmail.com>
2019-01-21 22:14:59 +0530

    added functionality to get user by its id

--- logindemo/src/main/java/com/raj/login/logindemo/controller/UserDetailsController.java
+++ logindemo/src/main/java/com/raj/login/logindemo/controller/UserDetailsController.java
@@ -1,8 +1,11 @@
 package com.raj.login.logindemo.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +15,8 @@ import com.raj.login.logidemo.model.UserDetails;
 import com.raj.login.logindemo.repository.UserDetailsRepository;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
+
 @RequestMapping("/user")
 public class UserDetailsController {
 
@@ -27,5 +32,15 @@ public class UserDetailsController {
 	public UserDetails saveUserDetails(@RequestBody UserDetails userDetails) {
 		return userDetailsRepository.save(userDetails);
 	}
+	
+	@RequestMapping("/showUser/{id}")
+	public Optional<UserDetails> findById(@PathVariable("id") long id) {
+		return userDetailsRepository.findById(id);		
+	}
 
+	/*@RequestMapping(value="/updateUser/", method=RequestMethod.PUT)
+	public UserDetails updateUser(UserDetails user) {
+		return userDetailsRepository.update(user) ;
+	}*/
+	
 }
